05384b9372b6dcfc3e96a939cc31d905ce0875ca
al361940 <al361940@aula1159act.sg.uji.es>
2019-05-07 11:13:04 +0200

    datepicker

--- TooPots/src/main/java/es/uji/TooPots/TooPotsConfiguration.java
+++ TooPots/src/main/java/es/uji/TooPots/TooPotsConfiguration.java
@@ -4,10 +4,17 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.Formatter;
 
 import es.uji.TooPots.dao.ActivityDao;
 import es.uji.TooPots.dao.ActivityTypeDao;
+import es.uji.TooPots.dao.CustomerDao;
 import es.uji.TooPots.dao.InstructorDao;
+
+import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 import javax.sql.DataSource;
 
@@ -34,4 +41,27 @@ public class TooPotsConfiguration {
 	public ActivityTypeDao activityTypeD() {
 		return new ActivityTypeDao();
 	}
+	
+	@Bean
+	public Formatter<LocalDateTime> localDateFormatterISO() {
+	   return new Formatter<LocalDateTime>() {
+		@Override
+		public String print(LocalDateTime object, Locale locale) {
+			// TODO Auto-generated method stub
+	           return DateTimeFormatter.ofPattern("dd-MM-yyyy").format(object); 
+		}
+
+		@Override
+		public LocalDateTime parse(String text, Locale locale) throws ParseException {
+			// TODO Auto-generated method stub
+	           return LocalDateTime.parse(text, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+		}
+	   };
+	}
+	
+	@Bean
+	public CustomerDao customerD() {
+		return new CustomerDao();
+	}
+
 }
