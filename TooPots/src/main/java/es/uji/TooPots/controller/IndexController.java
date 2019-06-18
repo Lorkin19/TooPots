d704bc637aef66c28d72e28f04584b1335f3f799
jaime <jaime@150.128.170.201>
2019-06-18 18:34:48 +0200

    mejora en vista, logo puesto en barra nav

---
+++ TooPots/src/main/java/es/uji/TooPots/controller/IndexController.java
@@ -0,0 +1,23 @@
+package es.uji.TooPots.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import es.uji.TooPots.model.UserDetails;
+
+@Controller
+public class IndexController {
+	@RequestMapping("/")
+    public String IndexInit(Model model) {
+		model.addAttribute("user", new UserDetails());
+        return "home.html"; 
+    }
+}
